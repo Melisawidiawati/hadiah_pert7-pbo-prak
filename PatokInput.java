17d7116eb56278fce023ac35bcd960dc37bca999
Melisawidiawati <90242961+Melisawidiawati@users.noreply.github.com>
2021-11-04 12:14:22 +0800

    Add files via upload

---
+++ PatokInput.java
@@ -0,0 +1,13 @@
+
+public class PatokInput {
+	protected byte tipe;
+	protected char blok;
+	
+	public void setTipe(byte tipe){
+	this.tipe=tipe;
+	}
+	
+	public void setBlok(char blok) {
+		this.blok=blok;
+	}
+}
